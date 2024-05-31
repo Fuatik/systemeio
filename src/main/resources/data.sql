d4492a48384457ab6bb9c7acaacbce10ea88db53
Fuat_Safiulin <fuatsafiulin@gmail.com>
2024-05-31 17:22:34 +0100

    data population

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,18 @@
-INSERT INTO product (id, name, price) VALUES (1, 'iPhone', 100);
-INSERT INTO product (id, name, price) VALUES (2, 'Headphones', 20);
-INSERT INTO product (id, name, price) VALUES (3, 'Case', 10);
+-- Insert products
+INSERT INTO product (id, name, price)
+VALUES (1, 'iPhone', 100),
+       (2, 'Headphones', 20),
+       (3, 'Case', 10);
 
 -- Insert coupons
-INSERT INTO coupon (id, code, discount, is_percentage) VALUES (1, 'P15', 15, TRUE);
-INSERT INTO coupon (id, code, discount, is_percentage) VALUES (2, 'P10', 10, TRUE);
-INSERT INTO coupon (id, code, discount, is_percentage) VALUES (3, 'F5', 5, FALSE);
+INSERT INTO coupon (id, code, discount, is_percentage)
+VALUES (4, 'D15', 15, TRUE),
+       (5, 'P10', 10, TRUE),
+       (6, 'F5', 5, FALSE);
 
-INSERT INTO tax (id, region, rate) VALUES (1, 'DE', 0.19);
-INSERT INTO tax (id, region, rate) VALUES (2, 'IT', 0.22);
-INSERT INTO tax (id, region, rate) VALUES (3, 'FR', 0.20);
-INSERT INTO tax (id, region, rate) VALUES (4, 'GR', 0.24);
+-- Insert taxes
+INSERT INTO tax (id, region, rate)
+VALUES (7, 'DE', 0.19),
+       (8, 'IT', 0.22),
+       (9, 'FR', 0.20),
+       (10, 'GR', 0.24);
