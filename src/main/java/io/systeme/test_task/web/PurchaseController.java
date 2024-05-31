eb11594b2046437308c95a9c0ef781fba00cf625
Fuat_Safiulin <fuatsafiulin@gmail.com>
2024-05-31 11:58:04 +0100

    added PurchaseController

--- src/main/java/io/systeme/test_task/web/PurchaseController.java
+++ src/main/java/io/systeme/test_task/web/PurchaseController.java
@@ -1,4 +1,51 @@
 package io.systeme.test_task.web;
 
+import io.systeme.test_task.exception.InvalidTaxNumberException;
+import io.systeme.test_task.exception.NotFoundException;
+import io.systeme.test_task.service.PaymentService;
+import io.systeme.test_task.service.PricingService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
 public class PurchaseController {
+
+    private final PricingService pricingService;
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/purchase")
+    public ResponseEntity<?> purchase(@RequestBody @Validated PurchaseRequest request) {
+        try {
+            double totalPrice = pricingService.calculateTotalPrice(request.productId, request.taxNumber, request.couponCode);
+
+            paymentService.payWithProcessor(totalPrice, request.paymentProcessor);
+
+            PurchaseResponse response = new PurchaseResponse(request, totalPrice, "Payment successful");
+
+            return ResponseEntity.ok(response);
+        } catch (InvalidTaxNumberException e) {
+            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                    HttpStatus.BAD_REQUEST,
+                    e.getMessage());
+            return ResponseEntity.badRequest().body(problemDetail);
+        } catch (NotFoundException e) {
+            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                    HttpStatus.NOT_FOUND,
+                    "Product with ID " + request.productId() + " not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
+        }
+    }
+
+    public record PurchaseRequest (@NotNull Integer productId, @NotBlank String taxNumber, String couponCode, @NotBlank String paymentProcessor) {}
+    public record PurchaseResponse (PurchaseRequest request, Double totalPrice, String message) {}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,11 @@ INSERT INTO product (id, name, price) VALUES (2, 'Headphones', 20);
 INSERT INTO product (id, name, price) VALUES (3, 'Case', 10);
 
 -- Insert coupons
-INSERT INTO coupon (id, code, discount, is_percentage) VALUES (1, 'D15', 15, TRUE);
+INSERT INTO coupon (id, code, discount, is_percentage) VALUES (1, 'P15', 15, TRUE);
 INSERT INTO coupon (id, code, discount, is_percentage) VALUES (2, 'P10', 10, TRUE);
 INSERT INTO coupon (id, code, discount, is_percentage) VALUES (3, 'F5', 5, FALSE);
+
+INSERT INTO tax (id, region, rate) VALUES (1, 'DE', 0.19);
+INSERT INTO tax (id, region, rate) VALUES (2, 'IT', 0.22);
+INSERT INTO tax (id, region, rate) VALUES (3, 'FR', 0.20);
+INSERT INTO tax (id, region, rate) VALUES (4, 'GR', 0.24);
