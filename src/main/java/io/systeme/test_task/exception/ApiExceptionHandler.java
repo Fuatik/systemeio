a61e70fb42b5f3bb1c73a03515b3b3eef8ec1bdf
Fuat_Safiulin <fuatsafiulin@gmail.com>
2024-06-05 21:53:54 +0200

    added ProblemDetails for validation errors

--- src/main/java/io/systeme/test_task/exception/ApiExceptionHandler.java
+++ src/main/java/io/systeme/test_task/exception/ApiExceptionHandler.java
@@ -8,24 +8,18 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 
+import java.net.URI;
 import java.util.List;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
-    @ExceptionHandler(BadRequestException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<?> handleBadRequestExceptions(Exception ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                HttpStatus.BAD_REQUEST,
-                ex.getMessage());
-        return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<?> handleMethodArgumentNotValidExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<?> handleMethodArgumentNotValidExceptions(MethodArgumentNotValidException ex, WebRequest request) {
 
         List<ObjectError> errors = ex.getBindingResult().getAllErrors();
         String defaultMessage = "Unknown error";
@@ -37,6 +31,21 @@ public class ApiExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
                 HttpStatus.BAD_REQUEST,
                 defaultMessage);
+        problemDetail.setType(URI.create("about:blank"));
+        problemDetail.setTitle("Bad Request");
+        problemDetail.setInstance(URI.create(((ServletWebRequest)request).getRequest().getRequestURI()));
+        return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handleBadRequestExceptions(Exception ex, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                ex.getMessage());
+        problemDetail.setType(URI.create("about:blank"));
+        problemDetail.setTitle("Bad Request");
+        problemDetail.setInstance(URI.create(((ServletWebRequest)request).getRequest().getRequestURI()));
         return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
     }
 }
